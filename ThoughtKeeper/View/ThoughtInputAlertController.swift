50dfa4fa7e146a78e67a3d24223a3284ce9a23c3
Sergey Shevchuk <seshevchuk11@gmail.com>
2019-12-01 23:57:45 +0300

    Added Alert Controller

---
+++ ThoughtKeeper/View/ThoughtInputAlertController.swift
@@ -0,0 +1,40 @@
+//
+//  ThoughtInputAlertController.swift
+//  ThoughtKeeper
+//
+//  Created by Sergey on 11/27/19.
+//  Copyright © 2019 Бизнес в стиле .RU. All rights reserved.
+//
+
+import UIKit
+
+protocol ThoughtInputDelegate: class {
+    func updateEnteredThought(text: String)
+}
+
+class ThoughtInputAlertController: UIAlertController {
+
+    weak var delegate: ThoughtInputDelegate?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createTextField()
+        addActions()
+    }
+
+    private func createTextField() {
+        self.addTextField { (textField) in
+            textField.placeholder = "Enter your new thought..."
+        }
+    }
+
+    private func addActions() {
+        let addAction = UIAlertAction(title: "Add", style: .default) { (_) in
+            if let textField = self.textFields?.first {
+                self.delegate?.updateEnteredThought(text: textField.text!)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        self.addAction(addAction)
+        self.addAction(cancelAction)
+    }
+}
